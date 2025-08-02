b40f997a4e4be2713842fdb217461a23a9358c80
Antonako1 <114558368+Antonako1@users.noreply.github.com>
2025-08-03 00:47:36 +0300

    Enhance cleanup logic in global.bat script
    Updated the DEL command to more precisely remove versioned ATRC and ATRCLua folders, and added cleanup steps for build artifacts in Lua and C# wrapper directories. This improves the script's ability to fully clean previous build outputs and related directories.

--- scripts/global.bat
+++ scripts/global.bat
@@ -3,25 +3,30 @@ pushd %cd%
 cd /D %~dp0
 cd ..
 
+:: Project root directory
 set PROJECT_ROOT=%cd%
 set FOLDER=ATRC
 set OUTPUT_DIR=%PROJECT_ROOT%\%FOLDER%
 SET PROJECT_FOLDER="%PROJECT_ROOT%\project"
-set /P VERSION=< "%PROJECT_FOLDER%\VERSION"
 
+SET /P VERSION=< "%PROJECT_FOLDER%\VERSION"
+
+:: Folder name: ATRC-<version>_<build_version>
 set FOLDER=%FOLDER%-%VERSION%
 
 if "%1" == "DEL" (
     for /d %%D in ("%PROJECT_ROOT%\%FOLDER%_*") do (
         RMDIR /S /Q "%%D"
     )
-    for /d %%D in ("%PROJECT_ROOT%\ATRCLua_*") do (
+    for /d %%D in ("%PROJECT_ROOT%\ATRCLua-%VERSION%_*") do (
         RMDIR /S /Q "%%D"
     )
     del "*.zip"
     del "*.tar.gz"
     del "*.7z"
     del "*.nupkg"
+
+    @REM if exist "out" rmdir /S /Q "out"
 )
 
 
@@ -47,9 +52,23 @@ set OUTPUT_DIR=%PROJECT_ROOT%\%FOLDER%
 if "%1" == "DEL" (
     REM Clear the previous output folder
     RMDIR /S /Q "%PROJECT_ROOT%/ATRC-*"
+    RMDIR /S /Q "%PROJECT_ROOT%/ATRCLua-*"
     DEL /Q/S/F %FOLDER%.zip
     DEL /Q/S/F %FOLDER%.tar
     DEL /Q/S/F %FOLDER%.tar.gz
     DEL /Q/S/F %FOLDER%.7z
+
+    cd /D %~dp0
+    cd Wrappers\Lua
+    if exist build rmdir /S /Q build
+    if exist libs\ATRC rmdir /S /Q libs\ATRC
+
+    cd ..\C#
+    if exist ATRCTest\bin rmdir /S /Q ATRCTest\bin
+    if exist ATRCTest\obj rmdir /S /Q ATRCTest\obj
+
+    if exist ATRCWrapper\bin rmdir /S /Q ATRCWrapper\bin
+    if exist ATRCWrapper\obj rmdir /S /Q ATRCWrapper\obj
+    if exist ATRCWrapper\nupkg rmdir /S /Q ATRCWrapper\nupkg
 )
 popd
